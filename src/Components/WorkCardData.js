d308613da2b3d19a07b5fbbe0b505e9b6896f9d4
Abhishek <abhisheksonawane1997@gmail.com>
2022-12-31 16:23:35 +0530

    Initial commit

---
+++ src/Components/WorkCardData.js
@@ -0,0 +1,30 @@
+import pro1 from '../Imgs/pro1.jpg';
+import pro2 from '../Imgs/pro2.jpg';
+import pro3 from '../Imgs/pro3.jpg';
+
+
+const WorkCardData = [
+  {
+    imgsrc: pro1,
+    title:'Traditional Hotel Chain',
+    text:'This website is build over react and uses redux also for global state managements, and react hooks.',
+    view:'https://react-redux-6k4rug5b3-abhisheksonawane97.vercel.app/?classId=3ca27094-495b-4a56-b553-85fedafc06b0&assignmentId=40a978bf-a057-48f7-a2d5-9ea0e78af74f&submissionId=264927a8-6734-b3ed-640c-65cf45d64535',
+    source:'https://github.com/abhishekSonawane97/reactReduxApp'
+  },
+  {
+    imgsrc: pro2,
+    title:'News Now live website',
+    text:'This website is build using html , css , and Javascript. this site has been build for Live news headiline. which uses source as BBC news api.',
+    view:'https://github.com/abhishekSonawane97/News-Now',
+    source:'https://github.com/abhishekSonawane97/News-Now'
+  },
+  {
+    imgsrc: pro3,
+    title:'PostMaster.',
+    text:'APIs being in the core of the software, required appropriate testing. This app provides api test functionality. build using html, bootstrap and javascript.',
+    view:'https://post-master-api-test-mc6uk3p8g-abhisheksonawane97.vercel.app',
+    source:'https://github.com/abhishekSonawane97/postMaster_API_Test'
+  },
+]
+
+export default WorkCardData;
